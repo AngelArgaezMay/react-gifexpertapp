6cc25ffc4b8295c32721996ca8ad223f83bfc48e
AngelArgaezMay <angelargaezmay@gmail.com>
2020-10-18 18:27:30 -0500

    docs agregado

---
+++ docs/precache-manifest.d9fc1880046e6cd7923513cec2345ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9f94ad609177f50bf169ccef8028b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3ceedaef27047d24d7c",
+    "url": "/static/css/main.593dec50.chunk.css"
+  },
+  {
+    "revision": "3bd7f3839b4867d08700",
+    "url": "/static/js/2.372dfa45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.372dfa45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ceedaef27047d24d7c",
+    "url": "/static/js/main.ad48442e.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
